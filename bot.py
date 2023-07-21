cf3c567e739951bc4aa9e2ffc382e48966fef66a
SURESH KIM <136443686+sureshkim@users.noreply.github.com>
2023-07-21 05:13:48 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,18 @@
-#    Copyright (c) 2021 Ayush
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
 from telethon import TelegramClient, events
 from decouple import config
 import logging
 from telethon.sessions import StringSession
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
 
 print("Starting...")
-
+# get your own values from my.telegram.org
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 SESSION = config("SESSION")
 FROM_ = config("FROM_CHANNEL")
 TO_ = config("TO_CHANNEL")
-
+# add channel or userchat id
 FROM = [int(i) for i in FROM_.split()]
 TO = [int(i) for i in TO_.split()]
 
@@ -35,19 +23,25 @@ except Exception as ap:
     print(f"ERROR - {ap}")
     exit(1)
 
+# Add forward from user chat to channel by using the forward_messages method
 @BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
-async def sender_bH(event):
-    if event.message.video:  # Only forward if the message contains a video
-        for i in TO:
-            try:
-                await BotzHubUser.send_message(
-                    i,
-                    event.message
-                )
-                print(f"Video forwarded from {event.chat_id} to {i}")
-            except Exception as e:
-                print(e)
+async def forward_userchat_to_channel(event):
+    if event.message.video:
+        try:
+            await BotzHubUser.forward_messages(TO, event.message)
+            print(f"Video forwarded from user chat {event.chat_id} to channels: {TO}")
+        except Exception as e:
+            print(e)
+
+# Add forward from channel to multiple channels using the forward_messages method
+@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
+async def forward_channel_to_channels(event):
+    if event.message.video:
+        try:
+            await BotzHubUser.forward_messages(TO, event.message)
+            print(f"Video forwarded from channel {event.chat_id} to channels: {TO}")
+        except Exception as e:
+            print(e)
 
 print("Bot has started.")
 BotzHubUser.run_until_disconnected()
-    
